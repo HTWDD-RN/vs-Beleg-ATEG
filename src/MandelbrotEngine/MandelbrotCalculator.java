d540f4651bc13efd2532bece18232f53649d7d12
Tobias Kroll <tobias.kroll2@stud.htw-dresden.de>
2025-05-13 11:09:38 +0200

    Klassenbearbeitung

---
+++ src/MandelbrotEngine/MandelbrotCalculator.java
@@ -0,0 +1,28 @@
+package mandelbrotengine;
+
+/* Klasse für die mathematische Logik
+ * - Berechnung pro Punkt
+ * - Umrechnung von Pixelkoordinate zu komplexe Zahlen
+*/
+
+public class MandelbrotCalculator {
+
+public static int MandelbrotCalculator (double re, doiblee im, int maxIter) {
+
+    //Parameter:
+    // re : Realteil der komplexen Zahl c = re + i*im
+    // im : Imaginäranteil von c
+    // maxIter: Maximale Iterationen, die durchgeführt werden sollen
+    
+    double x = 0.0; //Realteil von z
+    double y = 0.0; // Imaginärteil von z
+    int iter = 0; //Zählt die Wiederholungen
+
+    
+    
+
+  }
+}
+
+
+
