48d7a14d246469a7a92599292660dd005b6deb74
Ahmad Ali Nabizada <s87068@htw-dresden.de>
2025-05-27 10:55:03 +0200

    controller interface erstellt

--- src/main/java/vs_beleg_ateg/worker/WorkerInterface.java
+++ src/main/java/vs_beleg_ateg/worker/WorkerInterface.java
@@ -1,8 +1,16 @@
 package vs_beleg_ateg.worker;
+package vs_beleg_ateg.controller;
 
+
+import java.awt.image.BufferedImage;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.awt.image.BufferedImage;
 
 public interface WorkerInterface extends Remote {
     TaskResult computeTask(Task task) throws RemoteException;
+}
+
+public interface ControllerInterface {
+    BufferedImage startCalculation();
 }
