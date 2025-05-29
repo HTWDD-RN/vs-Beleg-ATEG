45f00075e9674d73c97d375441014fe2a623ecf0
Ahmad Ali Nabizada <s87068@htw-dresden.de>
2025-05-29 18:37:33 +0200

    controller aktualisiert

--- src/main/java/vs_beleg_ateg/controller/Controller.java
+++ src/main/java/vs_beleg_ateg/controller/Controller.java
@@ -1,17 +1,15 @@
 package vs_beleg_ateg.controller;
 import java.awt.Color;
 import vs_beleg_ateg.gui.GUI;
-import vs_beleg_ateg.worker.*;
 
 public class Controller {
-    int WORKER = 4;
-
     private int imageWidth, imageHeight;
     private double zoomPointX, zoomPointY;
     private double zoomFactor;
     private int stepCount;
     private int maxIterations;
     private GUI gui;
+    double xmin = -1.666, xmax = 1, ymin = -1, ymax = 1;
 
     public Controller(int imageWidth, int imageHeight, double zoomPointX, double zoomPointY, double zoomFactor, int stepCount, int maxIterations, int workerCount, GUI gui) {
         this.imageWidth = imageWidth;
@@ -24,29 +22,47 @@ public class Controller {
         this.gui = gui;
     }
 
+    //TODO
+    //while round                               OK
+    //ci cr, zoom berechnen                     OK
+    //schick werte an der worker                OK
+    //fertig                                    OK
 
     public void startComputation(){
-        Color[][] c = new Color[imageWidth][imageHeight];
-        double xmin = -1.666, xmax = 1, ymin = -1, ymax = 1;
 
+        int thread_sum = 4;
+        Thread[] threads = new Thread[thread_sum];
+
+        int x_length = imageWidth/thread_sum;
+        Color[][] bild = new Color[imageWidth][imageHeight];
+        
         for (int i = 1; i < stepCount; i++) { // Round loop
-            //für test
-            System.out.println(xmin + "- " + xmax + "- " + ymin + "- " + ymax + "-" + zoomPointX + "-" + zoomPointY);
+            for (int j = 0; j < thread_sum; j++) {
+                final int x_start = x_length * j;
+                final int x_stop = (j == thread_sum - 1) ? imageWidth : x_start + x_length;
 
-            //TODO
-            //schicke Variables an Workers
-            TaskResult workerResults[];
-            for(int j = 0;j < WORKER;j++){
-                Task task = new Task(xmin, ymin,xmax, ymax,imageWidth,imageHeight,i);
-                WorkerImpl worker = new WorkerImpl(task);
-                workerResults[j] = worker.computeTask(task);
-                };
+                threads[j] = new Thread(() -> {
+                    Color[][] bild_teil = bild_rechnen_worker(maxIterations, imageWidth, imageHeight, xmin, xmax, ymin, ymax, x_start, x_stop);
 
+                    for (int x = x_start; x < x_stop; x++) {
+                        for (int y = 0; y < imageHeight; y++) {
+                            bild[x][y] = bild_teil[x][y];
+                        }
+                    }
+                });
+                threads[j].start();
             }
-            c = bild_rechnen_worker(maxIterations, imageWidth, imageHeight, xmin, xmax, ymin, ymax);
+
+            for (Thread t : threads) {
+                try {
+                    t.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
 
             //zeichne das Bild
-            gui.givePixelData(c, imageWidth, imageHeight);
+            gui.givePixelData(bild, imageWidth, imageHeight);
 
             double xdim = xmax - xmin;
             double ydim = ymax - ymin;
@@ -54,6 +70,11 @@ public class Controller {
             xmax = zoomPointX + xdim / 2 / zoomFactor;
             ymin = zoomPointY - ydim / 2 / zoomFactor;
             ymax = zoomPointY + ydim / 2 / zoomFactor;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -61,8 +82,10 @@ public class Controller {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     //MUSS AUF WORKER VERSCHOBEN WERDEN
-    public Color[][] bild_rechnen_worker(int max_iter, int xpix, int ypix, double xmin, double xmax, double ymin, double ymax) {
-        Color[][] colors = new Color[xpix][ypix]; // [y][x]
+    private Color[][] bild_rechnen_worker(int max_iter, int xpix, int ypix, double xmin, double xmax, double ymin, double ymax, int x_start, int x_stop) {
+        //TODO: MUSS AUF WORKER x_start, x_stop IMPLEMENTIEREN!!!
+        
+        Color[][] colors = new Color[xpix][ypix];
 
         for (int y = 0; y < ypix; y++) {
             double c_im = ymin + (ymax - ymin) * y / ypix;
@@ -84,7 +107,7 @@ public class Controller {
     }
 
     //MUSS AUF WORKER VERSCHOBEN WERDEN
-    public int calc(int max_iter, double cr, double ci) {
+    private int calc(int max_iter, double cr, double ci) {
         int iter;
         double zr = 0, zi = 0, zr2 = 0, zi2 = 0, zri = 0, betrag = 0;
         for (iter = 0; iter < max_iter && betrag <= 4.0; iter++) {
