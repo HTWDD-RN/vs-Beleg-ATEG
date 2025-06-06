b89ba82e7bea559c6839831cbd0e7353c70ef78e
Ahmad Ali Nabizada <s87068@htw-dresden.de>
2025-06-06 13:51:42 +0200

    comment Controllerinterface

--- src/main/java/vs_beleg_ateg/controller/ControllerInterface.java
+++ src/main/java/vs_beleg_ateg/controller/ControllerInterface.java
@@ -1,4 +1,4 @@
-public class ControllerInterface {
+/* public class ControllerInterface {
     void registerWorker(Worker worker)throws RemoteException;
     int getWorkerCount() throws RemoteException;
-}
+} */
