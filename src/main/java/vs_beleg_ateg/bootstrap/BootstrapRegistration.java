98b53f664e83d5e68c419c727d45f53322e743e2
Computer-Freak-2004 <89689115+Computer-Freak-2004@users.noreply.github.com>
2025-06-07 12:15:42 +0200

    Ermittle korrekte IP Adresse für Server

--- src/main/java/vs_beleg_ateg/bootstrap/BootstrapRegistration.java
+++ src/main/java/vs_beleg_ateg/bootstrap/BootstrapRegistration.java
@@ -1,5 +1,7 @@
 package vs_beleg_ateg.bootstrap;
 import java.rmi.registry.LocateRegistry;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.rmi.*;
 
 public class BootstrapRegistration
@@ -8,6 +10,17 @@ public class BootstrapRegistration
     {
         try
         {
+            // Dynamisch die IP bestimmen, die für Internetverbindungen verwendet wird
+            String ip;
+            try (final DatagramSocket socket = new DatagramSocket()) {
+                socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+                ip = socket.getLocalAddress().getHostAddress();
+            }
+
+            // Diese IP für RMI setzen
+            System.setProperty("java.rmi.server.hostname", ip);
+            System.out.println("RMI Server hostname gesetzt auf: " + ip);
+
             LocateRegistry.createRegistry(1099);
             Naming.rebind("Bootstrap", new BootstrapImpl());
             System.out.println("Bootstrap-Server ist gestartet");
